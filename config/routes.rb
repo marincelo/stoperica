9c507b7c3d149fafb4fa50d6b000a9d98c71c981
Marin Bareta <marin.bareta@hotmail.com>
2018-11-21 21:43:55 +0100

    Show a redirect to a new site

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,5 @@
 Rails.application.routes.draw do
-  resources :leagues, path: :natjecanja, param: :slug
-  resources :pools
   devise_for :users
   root to: 'dashboard#info'
-
-  get '/timing' => 'dashboard#index'
-  get '/live' => 'dashboard#live'
-  post '/timesync' => 'dashboard#timesync'
   get '/info' => 'dashboard#info'
-  get '/terms' => 'dashboard#terms'
-  get '/izjava' => 'dashboard#waiver'
-
-  resources :clubs
-  resources :categories
-  resources :race_results do
-    collection do
-      match :from_device, via: [:get, :post]
-      post :from_timing
-      post :from_climbing
-      delete :destroy_from_timing
-    end
-  end
-  resources :races do
-    collection do
-      get :get_live
-    end
-    member do
-      get :assign_positions
-      get :embed
-    end
-  end
-  resources :racers do
-    collection do
-      get :login, to: 'racers#login_form'
-      post :login
-      get :search
-    end
-  end
-  resources :start_numbers do
-    collection do
-     get :start_time
-    end
-  end
 end
