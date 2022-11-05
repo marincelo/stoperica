10f2b1017624612a145e8f76ab6da74fdba78563
Marin Bareta <marin.bareta@hotmail.com>
2022-11-05 18:50:35 +0100

    Add new categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ApplicationRecord
   belongs_to :race
   has_many :race_results
   enum category: %i[zene u16 16-20 20-30 30-40 40-50 50 muskarci u9m u9w u11m u11w u13m u13w u15m u15w u17
-                    17-19 19-30 zeneu30 zene30 ebikem ebikez seniori juniori mladi]
+                    17-19 19-30 zeneu30 zene30 ebikem ebikez seniori juniori mladi u7m u7z]
 
   def started?
     race_results.count.positive? && race_results.first.started_at.present?
--- app/views/races/_race.json.jbuilder
+++ app/views/races/_race.json.jbuilder
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-json.extract! race, :id, :name, :date, :laps, :easy_laps, :created_at, :updated_at, :started_at, :ended_at
+json.extract! race, :id, :name, :date, :laps, :easy_laps, :created_at, :updated_at, :started_at, :ended_at, :picture_url
 json.url race_url(race, format: :json)
